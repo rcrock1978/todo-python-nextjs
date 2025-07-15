188ff6fbf3394669dfcc961da88520a40de67c52
Raymund C <rcrock1978@gmail.com>
2025-07-15 16:21:36 +0800

    init docker

--- todo-app/backend/tasks.py
+++
@@ -1,9 +0,0 @@
-from celery import Celery
-import os
-
-REDIS_URL = os.getenv("REDIS_URL", "redis://redis:6379/0")
-celery = Celery('tasks', broker=REDIS_URL)
-
-@celery.task
-def add(x, y):
-    return x + y
